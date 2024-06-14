687176d1098152f6896bea27fa0b3c9db083b4cb
ohseung.kang@rapeech.com <ohseung.kang@rapeech.com>
2024-06-14 18:21:55 +0900

    OK

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/firefox2.png");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+};
+
+let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+
+function setUserName() {
+    let myName = prompt("Please enter your name.");
+    if (!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla is cool, " + myName;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+};
