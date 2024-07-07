a41a65e9fdb515e9cae2ce40b159d69581f838e4
ohseung.kang@rapeech.com <ohseung.kang@rapeech.com>
2024-07-07 20:11:18 +0900

    호출 완료

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -1,30 +1,36 @@
 function cal() {
     let customerData = document.getElementById("customerData").value;
     // alert(customerData);
-    ChangeCustomerData(customerData);
+    const normalizeData = NormalizeCustomerData(customerData);
 
     let countPerPage = document.getElementById("countPerPage").value;
     // console.log(countPerPage);
 
-    // let requestData = makeRequestData(customerData, countPerPage);
+    let requestData = makeRequestData(normalizeData, countPerPage);
 
-    // const url = "http://testkos.duckdns.org/hae/posts";
-    // postData(url)
-    // or
-    // logJSONData();
+    // const url = "http://testkos.duckdns.org/";
+    const url = "http://127.0.0.1:8080/value";
+    postData(url, requestData).then(x => {
+        console.log("success:", x);
+        const result = document.getElementById("result");
+        result.hidden = false;
+        result.value = JSON.stringify(x);
+    })
+
 };
 
 function makeRequestData(customerData, countPerPage) {
-
+    const reqData = { customerData, countPerPage};
+    return reqData;
 }
 
-function ChangeCustomerData(data) {
-    let result = new Array();
-    data = data.trim();
+function NormalizeCustomerData(input) {
+    let result = Array();
     // console.log(data.length);
-    data.split("\n").forEach(row => {
+    input.split("\n").forEach(row => {
+        // console.log("before:" + row);
         row = row.trim();
-        // console.log(row);
+        // console.log("after:" + row);
         if (row != "") {
             data = row.split("\t", 3);
             // console.log(data);
@@ -36,38 +42,26 @@ function ChangeCustomerData(data) {
 }
 
 // POST 메서드 구현 예제
-async function postData(url = "", data = {}) {
-    // const otherParam = {
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     }
-    // }
-    // 옵션 기본 값은 *로 강조
-    const response = await fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE 등
-        mode: "no-cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data), // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
-    });
-    // console.log(response);
-    return response.json(); // JSON 응답을 네이티브 JavaScript 객체로 파싱
-}
+async function postData(url = "", data) {
+    let result;
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data), // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
+        });
 
-// postData("https://example.com/answer", { answer: 42 }).then((data) => {
-//     console.log(data); // JSON 데이터가 `data.json()` 호출에 의해 파싱됨
-// });
-// async function logJSONData() {
-//     const response = await fetch("http://testkos.duckdns.org/");
-//     const jsonData = await response.json();
-//     console.log(jsonData);
-// }
+        result = await response.json();
+        // console.log("success:", result);
+
+    } catch (error) {
+        console.error("fail:", error);
+    }
+
+    return result;
+}
 
 // main
 const calcuateButton = document.getElementById("calculate");
